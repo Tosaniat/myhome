1b88f2417ad3ca3b03fb3f10bd672e5e2e0a05ab
Tosaniat <18357315457@163.com>
2025-09-30 17:27:05 +0800

    first commit

---
+++ src/components/MoreContent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="more-content">
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;《十六字令三首》<br />
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;毛泽东<br />
+    山，快马加鞭未下鞍。惊回首，离天三尺三。<br />
+
+    山，倒海翻江卷巨澜。奔腾急，万马战犹酣。<br />
+
+    山，刺破青天锷未残。天欲堕，赖以拄其间。<br />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.more-content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+  width: 100%;
+  height: 100%;
+}
+</style>
